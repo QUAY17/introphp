c40745b5046f440116e8f69c54369cc33efb3d46
Jennifer Minnich <jminniecc@gmail.com>
2017-01-05 17:43:03 -0700

    adding ch 2 (name)
    & (number)
    
    adding first & last name
    
    favorite book

--- ch2.php
+++ ch2.php
@@ -1,7 +1,21 @@
 <?php
 
+
 $firstName = "Barbie Ann";
 	$number = 17;
 
-		echo $firstName;
-		echo $number;
+		//echo $number, ' ', $firstName;
+		//print $number;
+
+# this is a comment
+
+$firstName = "Jennifer"; //this stores my first name
+
+$lastName = "Minnich"; #this stores my last name
+
+//echo $firstName;
+//echo ' ';
+//echo $lastName;
+
+$book = "Doris Lessing's nobel prize winning book on Communism in the Eastern Europe";
+echo $book;
